dec3724fe8e0cff6f11454bc477ac454754beeb1
pedago <pedago@pcalb-mm0410.univ-jfc.fr>
2019-10-23 16:44:13 +0200

    SUP

---
+++ src/main/java/DaoCopy.java
@@ -0,0 +1,20 @@
+
+import javax.sql.DataSource;
+import simplejdbc.DAO;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pedago
+ */
+public class DaoCopy extends DAO{
+    public DaoCopy(DataSource datasource){
+        super(datasource);
+    }
+    
+}
